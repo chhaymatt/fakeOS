e45d082ff58b58bdc8541bc815371f713c11e49c
Matthew Chhay <chhaymatt@gmail.com>
2022-10-11 16:27:42 +1100

    Removed parameters from time function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DATE AND TIME
 const timeDisplay = document.getElementById("timeDisplay");
-let this12 = true;
-const time = (display12) => {
+// let this12 = true;
+const time = () => {
     
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -15,7 +15,7 @@ const time = (display12) => {
     const m = String(d.getMinutes()).padStart(2,0); // 0-59
     let h = String(d.getHours()); // 0-23
 
-    //let display12 = false;
+    let display12 = true;
     let displayYear = false;
     let displayFullDay = false;
     let displayFullMonth = false;
@@ -45,11 +45,11 @@ const time = (display12) => {
         ampm = "";
     }
 
-    timeDisplay.innerHTML = `${display12} ${dayFull} ${dayNumber} ${month} ${year} ${h}:${m}:${s} ${ampm}`;
+    timeDisplay.innerHTML = `${dayFull} ${dayNumber} ${month} ${year} ${h}:${m}:${s} ${ampm}`;
  
 };
 
-setInterval(time(this12), 1000); // Refresh time every 1000 miliseconds (1 second)
+setInterval(time, 1000); // Refresh time every 1000 miliseconds (1 second)
 
 // MENU BAR
 const appleButton = document.getElementById("appleButton");
