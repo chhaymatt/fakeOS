f38977dede46de6968e2684eddb7a8b78f9ec545
Matthew Chhay <chhaymatt@gmail.com>
2022-10-11 14:49:04 +1100

    Added timeMenu and draggable Finder window

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DATE AND TIME
-let timeDisplay = document.getElementById("timeDisplay");
+const timeDisplay = document.getElementById("timeDisplay");
 
 const time = () => {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -19,7 +19,7 @@ const time = () => {
     } else {
         ampm = "pm";
     }
-    console.log(dayNumber);
+    
     timeDisplay.innerHTML = `${dayFull} ${dayNumber} ${month} ${year} ${hours[h]}:${m}:${s} ${ampm}`;
 };
 
@@ -50,6 +50,10 @@ const toggleEditMenu = () => {
     menu[3].classList.toggle("hidden");
     editButton.classList.toggle("highlight");
 };
+const toggleTimeMenu = () => {
+    menu[4].classList.toggle("hidden");
+    timeDisplay.classList.toggle("highlight");
+};
 
 
 // MENU CLICK
@@ -57,6 +61,12 @@ appleButton.addEventListener("click",toggleAppleMenu);
 titleButton.addEventListener("click",toggleTitleMenu);
 fileButton.addEventListener("click",toggleFileMenu);
 editButton.addEventListener("click",toggleEditMenu);
+timeDisplay.addEventListener("click",toggleTimeMenu);
+
+
+// TIME MENU & CLICKS
+const hourButton = document.getElementById("hourButton");
+// hourButton.addEventListener("click",toggleHour);
 
 ///  APPS
 const finderButton = document.getElementById("finderButton");
@@ -113,9 +123,58 @@ const toggleFullscreenWeatherApp = () => {
 const toggleFullscreenMusicApp = () => {
     app[2].classList.toggle("fullscreen");
 }
-const fullscreenFinderButton = document.getElementById("fullscreenFinderButton");
+
+
+// const fullscreenFinderButton = document.getElementById("fullscreenFinderButton");
 const fullscreenWeatherButton = document.getElementById("fullscreenWeatherButton");
 const fullscreenMusicButton = document.getElementById("fullscreenMusicButton");
-fullscreenFinderButton.addEventListener("click",toggleFullscreenFinderApp);
+// fullscreenFinderButton.addEventListener("click",toggleFullscreenFinderApp);
 fullscreenWeatherButton.addEventListener("click",toggleFullscreenWeatherApp);
 fullscreenMusicButton.addEventListener("click",toggleFullscreenMusicApp);
+
+
+// Only making Finder draggable https://www.w3schools.com/howto/howto_js_draggable.asp
+// Make the DIV element draggable:
+dragElement(document.getElementById("finder"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
