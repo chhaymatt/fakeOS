1c57df309135776714562da135e08861004b023c
Matthew Chhay <chhaymatt@gmail.com>
2022-10-12 16:41:23 +1100

    Fixed closing other menus and refined draggable

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ let displaySeconds = false;
 
 const closeTimeMenu = () => {
     menu[4].classList.toggle("hidden");
-    timeDisplay.classList.toggle("highlight");
 };
 
 const toggleHour = () => {
@@ -128,23 +127,50 @@ const menu = document.getElementsByClassName("dropdown__group");
 // MENU HANDLERS
 const toggleAppleMenu = () => {
     menu[0].classList.toggle("hidden");
-    appleButton.classList.toggle("highlight");
+    
+    // Close other menus
+    menu[1].classList.add("hidden");
+    menu[2].classList.add("hidden");
+    menu[3].classList.add("hidden");
+    menu[4].classList.add("hidden");
+
 };
 const toggleTitleMenu = () => {
     menu[1].classList.toggle("hidden");
-    titleButton.classList.toggle("highlight");
+
+    // Close other menus
+    menu[0].classList.add("hidden");
+    menu[2].classList.add("hidden");
+    menu[3].classList.add("hidden");
+    menu[4].classList.add("hidden");
 };
 const toggleFileMenu = () => {
     menu[2].classList.toggle("hidden");
-    fileButton.classList.toggle("highlight");
+
+    // Close other menus
+    menu[0].classList.add("hidden");
+    menu[1].classList.add("hidden");
+    menu[3].classList.add("hidden");
+    menu[4].classList.add("hidden");
 };
 const toggleEditMenu = () => {
     menu[3].classList.toggle("hidden");
-    editButton.classList.toggle("highlight");
+
+    // Close other menus
+    menu[0].classList.add("hidden");
+    menu[1].classList.add("hidden");
+    menu[2].classList.add("hidden");
+    menu[4].classList.add("hidden");
 };
 const toggleTimeMenu = () => {
     menu[4].classList.toggle("hidden");
-    timeDisplay.classList.toggle("highlight");
+
+    // Close other menus
+    menu[0].classList.add("hidden");
+    menu[1].classList.add("hidden");
+    menu[2].classList.add("hidden");
+    menu[3].classList.add("hidden");
+
 };
 
 // MENU CLICK
@@ -246,58 +272,50 @@ const aboutPopupButton = document.getElementById("aboutPopupButton");
 const closeAboutPopup = document.getElementById("closeAboutPopup");
 
 const toggleAboutPopup = () => {
+    menu[0].classList.add("hidden");
+    appleButton.classList.remove("highlight");
     pop[0].classList.toggle("hidden");
 }
 
 aboutPopupButton.addEventListener("click",toggleAboutPopup);
 closeAboutPopup.addEventListener("click",toggleAboutPopup);
 
-
-
 // Source: https://www.w3schools.com/howto/howto_js_draggable.asp
-// Make the DIV element draggable:
+const dragElement = (elmnt) => {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    const elementDrag = (e) => {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    const closeDragElement = () => {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
 dragElement(document.getElementById("finder"));
 dragElement(document.getElementById("weather"));
 dragElement(document.getElementById("music"));
-
-function dragElement(elmnt) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "Header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
